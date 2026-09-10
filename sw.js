ee5f41a9185ef54af87bb4949173793082cfd00f
gzpm4zphr7-gif <gzpm4zphr7@privaterelay.appleid.com>
2026-09-10 13:32:23 +0100

    Update service worker to version 5 with new icons

--- sw.js
+++ sw.js
@@ -1,5 +1,39 @@
-const CACHE="tower-rush-v1";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-180.png","./icon-512.png"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request)))});
+const CACHE = "tower-rush-v5";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-180.svg",
+  "./icon-512.svg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method === "GET") {
+    event.respondWith(
+      caches.match(event.request).then(
+        cached => cached || fetch(event.request)
+      )
+    );
+  }
+});
